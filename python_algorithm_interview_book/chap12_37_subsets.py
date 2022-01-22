22e0a210168d1d4d062208b967fb80860802107e
alisyabob <sjs939883@gmail.com>
2022-01-23 03:28:19 +0900

    [ADD] 2022-01-22

--- python_algorithm_interview_book/chap12_37_subsets.py
+++ python_algorithm_interview_book/chap12_37_subsets.py
@@ -1,20 +1,23 @@
 from typing import List
 
-
 class test:
     def subsets(self, nums: List[int]) -> List[int]:
-        res=[]
+
+        # 결과를 받을 빈배열 생성
+        res = []
 
         def dfs(index, path):
             res.append(path)
-            print(res)
-            for i in range(index,len(nums)):
-                print(",,,,,",i+1,path+[nums[i]])
-                dfs(i+1,path+[nums[i]])
 
-        dfs(0,[])
+            print("res", res)
+            for i in range(index, len(nums)):
+                print("-------------------------------")
+                print("iiii",i)
+                dfs(i + 1, path + [nums[i]])
+                print("path", path, "nums", [nums[i]])
+
+        dfs(0, [])
         return res
 
-
 test = test()
-test.subsets([1,2,3])
+test.subsets([1, 2, 3])
